2d82c43c747d62642dceac7bbdbafca1d177938b
Pitjiodo <32564196+Pitjiodo@users.noreply.github.com>
2018-04-16 21:03:14 +0200

    creating post
    js for creating post only needs to stored

--- assets/js/createpost.js
+++ assets/js/createpost.js
@@ -4,7 +4,8 @@ document.querySelector(".forum-button").addEventListener("click", myFunction);
 
 function myFunction() {
     document.querySelector(".forum-button").innerHTML = "Creating thread";
-        
+    
+    // create post edit:
     var div = document.createElement("div");
     var title = document.createElement("input");
     var post = document.createElement("button");
@@ -34,11 +35,46 @@ function myFunction() {
 }
 
 function postFunktion() {
+    // store data:
     document.querySelector(".newpostbtn").innerHTML = "Posting";
     var title = document.querySelector(".newtitle").value;
-    var Text = document.querySelector(".newtext").value;
+    var text = document.querySelector(".newtext").value;
     
     localStorage.setItem("Title", title);
-    localStorage.setItem("text", Text);
+    localStorage.setItem("Text", text);
     
+    
+    // create new post:
+    var div = document.createElement("div");
+    var title = document.createElement("h2");
+    var text = document.createElement("p");
+    
+    div.classList.add("postet");
+    title.classList.add("postettitle");
+    text.classList.add("postettext");
+    
+    document.querySelector(".content-forum").appendChild(div);
+    div.appendChild(title);
+    div.appendChild(text);
+    
+    var titletext = document.createTextNode(localStorage.getItem("Title"));
+    title.appendChild(titletext);
+    
+    var posttext = document.createTextNode(localStorage.getItem("Text"));
+    text.appendChild(posttext);
+
+        
+    var container = document.querySelector(".content-forum");
+    var firstelm = document.querySelector(".post-forum");
+    container.insertBefore(div,firstelm);
+    
+    // Store Post:
+    
+    
+    // Remove old:
+    var oldDiv = document.querySelector(".newpost")
+    document.querySelector(".content-forum").removeChild(oldDiv);
 }
+
+
+
