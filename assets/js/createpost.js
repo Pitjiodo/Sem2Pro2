129abeda9ad080f2a883fd33260296d561aef4cf
Pitjiodo <32564196+Pitjiodo@users.noreply.github.com>
2018-04-16 18:58:49 +0200

    js forum

--- assets/js/createpost.js
+++ assets/js/createpost.js
@@ -2,7 +2,25 @@ document.querySelector(".forum-button").addEventListener("click", myFunction);
 
 
 function myFunction() {
-    document.querySelector(".forum-button").innerHTML = "Creating thread";
+    document.querySelector(".forum-button");
     
+        
+    var div = document.createElement("div");
+    var title = document.createElement("input");
+    var post = document.createElement("button");
+    var text = document.createElement("input");
     
+    div.classList.add("newpost");
+    title.classList.add("newtitle");
+    text.classList.add("newtext");
+    post.classList.add("newpostbtn");
+    
+    document.querySelector(".content-forum").appendChild(div);
+    div.appendChild(title);
+    div.appendChild(text);
+    div.appendChild(post);
+    
+    document.querySelector(".newtitle").placeholder = "Title...";
+    document.querySelector(".newtext").placeholder = "Text...";
+    document.querySelector(".newpostbtn").innerHTML = "Post";
 }
