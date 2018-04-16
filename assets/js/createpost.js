a6a5ce431f3f25c11007d52ee1d3ff896cd5af90
Pitjiodo <32564196+Pitjiodo@users.noreply.github.com>
2018-04-16 20:25:22 +0200

    posting js

--- assets/js/createpost.js
+++ assets/js/createpost.js
@@ -1,15 +1,14 @@
 document.querySelector(".forum-button").addEventListener("click", myFunction);
 
 
+
 function myFunction() {
-    document.querySelector(".forum-button");
-    
+    document.querySelector(".forum-button").innerHTML = "Creating thread";
         
     var div = document.createElement("div");
     var title = document.createElement("input");
     var post = document.createElement("button");
     var text = document.createElement("input");
-    var
     
     div.classList.add("newpost");
     title.classList.add("newtitle");
@@ -24,8 +23,22 @@ function myFunction() {
     document.querySelector(".newtitle").placeholder = "Title...";
     document.querySelector(".newtext").placeholder = "Text...";
     document.querySelector(".newpostbtn").innerHTML = "Post";
-    
+
+        
     var container = document.querySelector(".content-forum");
     var firstelm = document.querySelector(".post-forum");
-    firstelm.insertBefore(div,)
+    container.insertBefore(div,firstelm);
+    
+    document.querySelector(".newpostbtn").addEventListener("click", postFunktion);
+    
+}
+
+function postFunktion() {
+    document.querySelector(".newpostbtn").innerHTML = "Posting";
+    var title = document.querySelector(".newtitle").value;
+    var Text = document.querySelector(".newtext").value;
+    
+    localStorage.setItem("Title", title);
+    localStorage.setItem("text", Text);
+    
 }
